68677a8fd71e7cee60ab206e63291501bc10b155
Provashish Roy <provashishroy95@gmail.com>
2022-04-09 12:20:49 +0600

    added some components

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import './Login.css';
 
-
-const Login = () => {
+const Register = () => {
     return (
         <div>
-            <h3>Please login</h3>
-            <div style={{ margin: '20px' }}>
-                <button>Google sign in</button>
-            </div>
+            <h4>Please Register Now</h4>
             <form>
                 <input type="text" name="name" placeholder='Enter your name..' /><br />
                 <input type="email" name="email" placeholder='Example@gmail.com' /><br />
-                <input type="submit" value="Login" />
+                <input type="password" /><br />
+                <input type="submit" value="Register" />
             </form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
